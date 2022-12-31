97945a647faed5e5bb0447b8f2473dfcecbf8d7d
enmanuel <enmanuelmd357@gmail.com>
2022-12-31 02:35:06 -0300

    fix settings

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -132,10 +132,11 @@ class AuthController extends Controller
         $form->display('username', trans('admin.username'));
         $form->text('name', trans('admin.name'))->rules('required');
         $form->image('avatar', trans('admin.avatar'));
-        $form->password('password', trans('admin.password'))->rules('confirmed|required');
-        $form->password('password_confirmation', trans('admin.password_confirmation'))->rules('required')
+        $form->password('password', trans('admin.password'))->rules('confirmed');
+        $form->password('password_confirmation', trans('admin.password_confirmation'))
             ->default(function ($form) {
-                return $form->model()->password;
+//                return $form->model()->password;
+                return '';
             });
 
         $form->setAction(admin_url('auth/setting'));
@@ -143,7 +144,7 @@ class AuthController extends Controller
         $form->ignore(['password_confirmation']);
 
         $form->saving(function (Form $form) {
-            if ($form->password && $form->model()->password != $form->password) {
+            if ($form->password !== '' && $form->model()->password != $form->password) {
                 $form->password = Hash::make($form->password);
             }
         });
